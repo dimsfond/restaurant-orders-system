5f404c1602638644bba8a7284bcf181e7079833b
dimsfond <176294793+dimsfond@users.noreply.github.com>
2025-12-06 00:08:07 +0200

    Add endpoints for creating customer, deleting him before he eats, listing present customers and marking him not present when he pays and leaves

--- app/main.py
+++ app/main.py
@@ -1,21 +1,47 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
-from app import models, database
+from app import database
+from models import Customer, Order, OrderItem, MenuItem
 
 app = FastAPI(title = "Restaurant orders API")
 
 def start_db():
     database.init_db()
 
-@app.post("/menu/")
-def createProduct(product: ProductCreate, db: Session = Depends(database.get_database)):
-    new_product = models.Products(name = product.name, price = product.price)
-    db.add(new_product)
+@app.post("/customers/", response_model = dict)
+def add_customer(table_number: int, db: Session = Depends(database.get_db)):
+    customer = Customer(table_number)
+    db.add(customer)
     db.commit()
-    db.refresh(new_product)
-    return new_product
+    db.refresh(customer)
+    return {id: customer.id, table_number: customer.table_number}
 
-@app.get("/menu/")
-def listProducts(db: Session = Depends(database.get_database)):
-    products = db.query(models.Products).all()
-    return products
+@app.delete("/customers/{id}", response_model = dict)
+def delete_customer(id: int, db: Session = Depends(database.get_db)):
+    customer = db.query(Customer).filter(Customer.id == id).first()
+    if not customer:
+        raise HTTPException(status_code = 404, detail = "Customer not found")
+    db.delete(customer)
+    db.commit()
+    return {"detail: Customer {id} left the restaurant without eating and he, along with his unprepared orders, were deleted"}
+
+@app.get("/customers/active", response_model = list[dict])
+def list_present_customers(db: Session = Depends(database.get_db)):
+    customers = db.query(Customer).filter(Customer.is_present == True).all()
+    return [
+        {
+            "id": customer.id,
+            "table_number": customer.table_number,
+        }
+        for customer in customers
+    ]
+
+@app.patch("customers/{id}/leave", response_model = dict)
+def customer_leaves(id: int, db: Session = Depends(database.get_db)):
+    customer = db.query(Customer).filter(Customer.id == id).first()
+    if not customer:
+        raise HTTPException(status_code = 404, detail = "Customer_not found")
+    customer.is_present = False
+    db.commit()
+    db.refresh(customer)
+    return {"detail": "Customer {id} has paid and left"}
