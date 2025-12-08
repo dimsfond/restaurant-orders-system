676e6ed525027d3f9fed38ca3b3261e19b4a83e5
dimsfond <176294793+dimsfond@users.noreply.github.com>
2025-12-08 22:04:40 +0200

    Rename app folder to my_app

--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -2,9 +2,9 @@ from fastapi.testclient import TestClient
 import pytest
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from app.models import Base, Customer, MenuItem
-from app.main import app
-from app.database import get_db
+from my_app.models import Base, Customer, MenuItem
+from my_app.main import app
+from my_app.database import get_db
 
 TEST_DATABASE_URL = "sqlite:///:memory:"
 engine = create_engine(TEST_DATABASE_URL, connect_args={"check_same_thread": False})
--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -1,8 +1,8 @@
 import pytest
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from app.models import Base, MenuItem, Order, OrderItem
-from app.utilities import compute_order_total
+from my_app.models import Base, MenuItem, Order, OrderItem
+from my_app.utilities import compute_order_total
 
 TEST_DATABASE_URL = "sqlite:///:memory:"
 engine = create_engine(TEST_DATABASE_URL, connect_args={"check_same_thread": False})
